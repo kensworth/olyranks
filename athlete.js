e35c6165445114216244db50432156faed3a214d
Ken Zhang <kyz211@nyu.edu>
2016-01-12 22:41:53 +0800

    updating athlete schema

---
+++ athlete.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var athleteSchema = new mongoose.Schema({
+  lifterId: { type: String, unique: true, index: true },
+  name: String,
+  nationality: String,
+  gender: String,
+  age: Number,
+  //best lifts, competitions?
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 },
+  reports: { type: Number, default: 0 },
+  random: { type: [Number], index: '2d' },
+  voted: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Athlete', characterSchema);
