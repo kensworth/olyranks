a906997ba4424d67e7f021cc9603648e073462f7
Ken Zhang <kyz211@nyu.edu>
2016-01-16 18:13:32 +0800

    updating crawler

--- crawler.js
+++ crawler.js
@@ -1,6 +1,110 @@
 var Crawler = require("crawler");
 var url = require('url');
 
+// Babel ES6/JSX Compiler
+require('babel-register');
+
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var async = require('async');
+var colors = require('colors');
+var mongoose = require('mongoose');
+var request = require('request');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var swig  = require('swig');
+var xml2js = require('xml2js');
+var _ = require('underscore');
+var util = require('util');
+
+var config = require('./config');
+var routes = require('./app/routes');
+var Character = require('./models/athlete');
+
+var app = express();
+
+mongoose.connect(config.database);
+mongoose.connection.on('error', function() {
+  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
+});
+
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/api/characters', function(req, res, next) {
+  var gender = req.body.gender;
+  var athleteName = req.body.name;
+  var athleteIdLookupUrl = 'http://www.iwrp.net/?view=contestant&id_zawodnik=' + athleteName;
+
+  var parser = new xml2js.Parser();
+
+  request.get(athleteIdLookupUrl, function(err, request, xml) {
+    if (err) return next(err);
+    parser.parseString(xml, function(err, parsedXml) {
+      
+      console.log(xml);
+      console.log('-------');
+      console.log(util.inspect(parsedXml, false, null));
+
+    });
+  });     
+});
+
+app.use(function(req, res) {
+  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirectLocation) {
+      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+        var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+        var page = swig.renderFile('views/index.html', { html: html });
+        res.status(200).send(page);
+    } else {
+      res.status(404).send('Page Not Found')
+    }
+  });
+});
+
+app.use(function(err, req, res, next) {
+  console.log(err.stack.red);
+  res.status(err.status || 500);
+  res.send({ message: err.message });
+});
+
+/**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function(socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+/*
 var c = new Crawler({
     maxConnections : 10,
     // This will be called for each crawled page
@@ -26,4 +130,4 @@ for(i = 1; i < 34000; i++) {
     c.queue({
         uri: search(i)
     });
-}
+}*/
